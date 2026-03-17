de76e07a821b2281b8ffd4c86174049998fe3fbe
Nadi25 <nadine-arzt@gmx.de>
2026-03-17 22:45:41 +0100

    combine all four plots into one panel

---
+++ Cooling_Warming_with_without_biomass_one_plot.R
@@ -0,0 +1,48 @@
+
+
+# Combine all four plots --------------------------------------------------
+
+# obviously need to run the other script first to get
+p_nb
+p_nb_warm
+p_bio_nb_species
+p_bio_nb_species_warm
+
+library(patchwork)
+
+final_plot <- 
+  ((p_nb + labs(title = "Cooling")) | 
+     (p_bio_nb_species + labs(title = "Cooling + biomass"))) /
+  ((p_nb_warm + labs(title = "Warming")) | 
+     (p_bio_nb_species_warm + labs(title = "Warming + biomass"))) +
+  plot_layout(guides = "collect") +
+  plot_annotation(
+    tag_levels = "A",
+    title = "Effects of temperature and competition on flower number NOR"
+  )
+
+final_plot
+
+
+final_plot <- 
+  final_plot +
+  plot_layout(guides = "collect") & 
+  theme(
+    legend.position = "bottom",
+    legend.direction = "horizontal"
+  )
+
+final_plot
+
+ggsave(filename = "Output/Biomass/Cooling_warming_with_without_biomass_NOR.png", 
+       plot = final_plot, 
+       width = 20, height = 18, units = "in")
+
+
+
+
+
+
+
+
+
