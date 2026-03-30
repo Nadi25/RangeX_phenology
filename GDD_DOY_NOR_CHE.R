c988e24e2460d30f048f1028a577fd69981386d6
Nadi25 <nadine-arzt@gmx.de>
2026-03-30 20:33:08 +0200

    GDD vs DOY

---
+++ GDD_DOY_NOR_CHE.R
@@ -0,0 +1,71 @@
+
+
+
+# GDD vs DOY --------------------------------------------------------------
+
+# plot gdd against calender day to see how warth is accumumlating
+
+source("Data_preparation_climate_station_CHE.R")
+source("Data_preparation_climate_station_NOR.R")
+
+
+climate_gdd_che$region <- "Switzerland"
+climate_gdd_pt$region <- "Norway"
+
+climate_gdd_nor <- climate_gdd_pt
+
+
+climate_all <- bind_rows(climate_gdd_che, climate_gdd_nor)
+
+
+climate_all <- climate_all |>
+  mutate(jday = lubridate::yday(date_measurement))
+
+
+
+cum_gdd_doy <- ggplot(climate_all,
+       aes(x = jday, y = GDD_cum,
+           color = site, linetype = region)) +
+  
+  geom_line(linewidth = 1.2) +
+  
+  labs(x = "Day of year",
+       y = "Cumulative temperature in GDD",
+       color = "Site",
+       linetype = "Site") +
+  
+  scale_color_manual(values = c("blue", "pink2"))
+cum_gdd_doy
+
+
+ggsave(filename = "Output/Onset/GDD_DOY_comparison_NOR_CHE.png", 
+       plot = cum_gdd_doy, width = 12, height = 10, units = "in")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
