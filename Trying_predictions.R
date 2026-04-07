41e1520618db140b41ba90497f288d70c8bb084b
Nadi25 <nadine-arzt@gmx.de>
2026-04-07 16:17:36 +0200

    predicting onsets for bud, flower, fruit

---
+++ Trying_predictions.R
@@ -0,0 +1,360 @@
+
+
+# Predictions for onset ---------------------------------------------------
+# https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#predictions-andor-confidence-or-prediction-intervals-on-predictions
+
+# Transplantation - budding -----------------------------------------------
+
+source("Effect_cooling_on_budding_onset_NOR_CHE.R")
+
+
+onset_bud_cool <- phenology_cool |> 
+  filter(phenology_stage == "No_Buds", value > 0) |>
+  group_by(region, site, treat_competition, species, block_ID, unique_plot_ID, 
+           unique_plant_ID, phenology_stage) |>
+  summarise(onset = min(jday, na.rm = TRUE), .groups = "drop") |>
+  # remove groups where budding never occurred
+  filter(is.finite(onset))
+
+
+m_onset_bud_cooling <- lmerTest::lmer(onset ~ region * site * treat_competition + 
+                                        (1|species) + (1|block_ID), 
+                                      data = onset_bud_cool)
+
+summary(m_onset_bud_cooling)
+
+
+
+# create new matrix for predicted data ------------------------------------
+newdat <- expand.grid(
+  region = c("Norway", "Switzerland"),
+  site = c("hi", "lo"),
+  treat_competition = c("with", "without"),
+  onset = 0
+)
+newdat
+
+
+
+# predict  ----------------------------------------------------------------
+newdat$onset <- predict(
+  m_onset_bud_cooling,
+  newdata = newdat,
+  re.form = NA 
+)
+newdat
+
+
+# make model matrix -------------------------------------------------------
+mm <- model.matrix(terms(m_onset_bud_cooling),newdat)
+mm
+
+
+pvar1 <- diag(mm %*% tcrossprod(vcov(m_onset_bud_cooling),mm))
+# tvar1 <- pvar1+VarCorr(m_onset_bud_cooling)$species[1]  ## must be adapted for more complex models
+
+
+# 2. EXTRACT RANDOM EFFECT VARIANCES
+# VarCorr returns variance-covariance matrices for each group
+var_species <- as.numeric(VarCorr(m_onset_bud_cooling)$species)
+var_block <- as.numeric(VarCorr(m_onset_bud_cooling)$block_ID)
+
+# 3. CALCULATE TOTAL VARIANCE
+# This is fixed-effect uncertainty + variance between sites + variance between blocks
+# If you want the interval for a NEW observation (individual point), add sigma(fm1)^2 as well
+tvar1 <- pvar1 + var_species + var_block + sigma(m_onset_bud_cooling)^2
+
+# 4. CALCULATE INTERVALS
+cmult <- 2 # is roughly twice standard error
+
+
+newdat <- data.frame(
+  newdat
+  , plo = newdat$onset-cmult*sqrt(pvar1) # fixed effects only, confidence interval
+  , phi = newdat$onset+cmult*sqrt(pvar1)
+  , tlo = newdat$onset-cmult*sqrt(tvar1) # takes fixed and random effects, prediction intervall
+  , thi = newdat$onset+cmult*sqrt(tvar1)
+)
+newdat
+
+
+
+# plot predicted onset ----------------------------------------------------
+pd <- position_dodge(width = 0.4) 
+
+p<- ggplot(newdat, aes(x=treat_competition, y=onset, 
+                       color=treat_competition, shape = site)) +
+  geom_point(position = pd)+
+  facet_wrap(~ region)+
+  geom_errorbar(aes(ymin= plo, ymax= phi), width=.2,
+                position = pd)+
+  scale_color_manual(values = c(
+    "with" = "#528B8B",
+    "without" = "#CD950C"
+  ))+
+  labs(
+    x = "Biotic interactions",
+    y = "Predicted onset (budding)",
+    title = "Effect of transplantation on budding onset across regions"
+  )
+print(p)
+
+# so we use the CI with fixed effects only
+
+
+
+
+# Transplantation - flowering -----------------------------------------------
+
+source("Effect_cooling_on_flowering_onset_NOR_CHE.R")
+
+# calculate flowering onset ------------------------------------------------
+flowering_onset_n_c_cool <- phenology_cool |> 
+  filter(phenology_stage == "No_FloOpen", value > 0) |>
+  group_by(region, site, species, unique_plant_ID, block_ID, treat_competition) |>
+  summarise(onset = min(jday, na.rm = TRUE), .groups = "drop") |>
+  # remove groups where flowering never occurred
+  filter(is.finite(onset))
+
+
+m_onset_n_c_cooling <- lmerTest::lmer(onset ~ region * site * treat_competition + 
+                                        (1|species) + (1|block_ID), 
+                                      data = flowering_onset_n_c_cool)
+summary(m_onset_n_c_cooling)
+
+
+
+# create new matrix for predicted data ------------------------------------
+newdat_cool_flower <- expand.grid(
+  region = c("Norway", "Switzerland"),
+  site = c("hi", "lo"),
+  treat_competition = c("with", "without"),
+  onset = 0
+)
+newdat_cool_flower
+
+
+
+# predict  ----------------------------------------------------------------
+newdat_cool_flower$onset <- predict(
+  m_onset_n_c_cooling,
+  newdata = newdat_cool_flower,
+  re.form = NA 
+)
+newdat_cool_flower
+
+
+# make model matrix -------------------------------------------------------
+mm_c_f <- model.matrix(terms(m_onset_n_c_cooling), newdat_cool_flower)
+mm_c_f
+
+
+pvar1_c_f <- diag(mm_c_f %*% tcrossprod(vcov(m_onset_n_c_cooling),mm_c_f))
+# tvar1 <- pvar1+VarCorr(m_onset_bud_cooling)$species[1]  ## must be adapted for more complex models
+
+
+# 2. EXTRACT RANDOM EFFECT VARIANCES
+# VarCorr returns variance-covariance matrices for each group
+var_c_f_species <- as.numeric(VarCorr(m_onset_n_c_cooling)$species)
+var_c_f_block <- as.numeric(VarCorr(m_onset_n_c_cooling)$block_ID)
+
+# 3. CALCULATE TOTAL VARIANCE
+# This is fixed-effect uncertainty + variance between sites + variance between blocks
+# If you want the interval for a NEW observation (individual point), add sigma(fm1)^2 as well
+tvar1_c_f <- pvar1_c_f + var_c_f_species + var_c_f_block + sigma(m_onset_n_c_cooling)^2
+
+# 4. CALCULATE INTERVALS
+cmult <- 2 # is roughly twice standard error
+
+
+newdat_cool_flower <- data.frame(
+  newdat_cool_flower
+  , plo = newdat_cool_flower$onset-cmult*sqrt(pvar1) # fixed effects only, confidence interval
+  , phi = newdat_cool_flower$onset+cmult*sqrt(pvar1)
+  , tlo = newdat_cool_flower$onset-cmult*sqrt(tvar1) # takes fixed and random effects, prediction intervall
+  , thi = newdat_cool_flower$onset+cmult*sqrt(tvar1)
+)
+newdat_cool_flower
+
+
+
+# plot predicted onset ----------------------------------------------------
+pd <- position_dodge(width = 0.4) 
+
+f<- ggplot(newdat_cool_flower, aes(x=treat_competition, y=onset, 
+                       color=treat_competition, shape = site)) +
+  geom_point(position = pd)+
+  facet_wrap(~ region)+
+  geom_errorbar(aes(ymin= plo, ymax= phi), width=.2,
+                position = pd)+
+  scale_color_manual(values = c(
+    "with" = "#528B8B",
+    "without" = "#CD950C"
+  ))+
+  labs(
+    x = "Biotic interactions",
+    y = "Predicted onset (flowering)",
+    title = "Effect of transplantation on budding onset across regions"
+  )
+print(f)
+
+
+
+# Transplantation - fruiting -----------------------------------------------
+
+source("Effect_cooling_on_fruiting_onset_NOR_CHE.R")
+
+# calculate fruiting onset ------------------------------------------------
+onset_fruit_cool <- phenology_cool |> 
+  filter(phenology_stage == "No_FloWithrd", value > 0) |>
+  group_by(region, site, treat_competition, species, block_ID, unique_plot_ID, 
+           unique_plant_ID, phenology_stage) |>
+  summarise(onset = min(jday, na.rm = TRUE), .groups = "drop") |>
+  # remove groups where fruits never occurred
+  filter(is.finite(onset))
+
+# model with region for fruiting onset lmer ----------------------------------
+m_onset_fruit_cooling <- lmerTest::lmer(onset ~ region * site * treat_competition + 
+                                          (1|species) + (1|block_ID), 
+                                        data = onset_fruit_cool)
+
+summary(m_onset_fruit_cooling)
+
+
+
+# create new matrix for predicted data ------------------------------------
+newdat_cool_fruit <- expand.grid(
+  region = c("Norway", "Switzerland"),
+  site = c("hi", "lo"),
+  treat_competition = c("with", "without"),
+  onset = 0
+)
+newdat_cool_fruit
+
+
+
+# predict  ----------------------------------------------------------------
+newdat_cool_fruit$onset <- predict(
+  m_onset_fruit_cooling,
+  newdata = newdat_cool_fruit,
+  re.form = NA 
+)
+newdat_cool_fruit
+
+
+# make model matrix -------------------------------------------------------
+mm_c_fr <- model.matrix(terms(m_onset_fruit_cooling), newdat_cool_fruit)
+mm_c_fr
+
+
+pvar1_c_fr <- diag(mm_c_fr %*% tcrossprod(vcov(m_onset_fruit_cooling),mm_c_fr))
+# tvar1 <- pvar1+VarCorr(m_onset_bud_cooling)$species[1]  ## must be adapted for more complex models
+
+
+# 2. EXTRACT RANDOM EFFECT VARIANCES
+# VarCorr returns variance-covariance matrices for each group
+var_c_fr_species <- as.numeric(VarCorr(m_onset_fruit_cooling)$species)
+var_c_fr_block <- as.numeric(VarCorr(m_onset_fruit_cooling)$block_ID)
+
+# 3. CALCULATE TOTAL VARIANCE
+# This is fixed-effect uncertainty + variance between sites + variance between blocks
+# If you want the interval for a NEW observation (individual point), add sigma(fm1)^2 as well
+tvar1_c_fr <- pvar1_c_fr + var_c_fr_species + var_c_fr_block + sigma(m_onset_fruit_cooling)^2
+
+# 4. CALCULATE INTERVALS
+cmult <- 2 # is roughly twice standard error
+
+
+newdat_cool_fruit <- data.frame(
+  newdat_cool_fruit
+  , plo = newdat_cool_fruit$onset-cmult*sqrt(pvar1) # fixed effects only, confidence interval
+  , phi = newdat_cool_fruit$onset+cmult*sqrt(pvar1)
+  , tlo = newdat_cool_fruit$onset-cmult*sqrt(tvar1) # takes fixed and random effects, prediction intervall
+  , thi = newdat_cool_fruit$onset+cmult*sqrt(tvar1)
+)
+newdat_cool_fruit
+
+
+
+# plot predicted onset ----------------------------------------------------
+pd <- position_dodge(width = 0.4) 
+
+fr <- ggplot(newdat_cool_fruit, aes(x=treat_competition, y=onset, 
+                                   color=treat_competition, shape = site)) +
+  geom_point(position = pd)+
+  facet_wrap(~ region)+
+  geom_errorbar(aes(ymin= plo, ymax= phi), width=.2,
+                position = pd)+
+  scale_color_manual(values = c(
+    "with" = "#528B8B",
+    "without" = "#CD950C"
+  ))+
+  labs(
+    x = "Biotic interactions",
+    y = "Predicted onset (fruiting)",
+    title = "Effect of transplantation on budding onset across regions"
+  )
+print(fr)
+
+
+
+
+
+
+# combine stages into one figure ------------------------------------------
+
+stage_colors <- c(
+  "Budding"   = "#4F9EC9",   
+  "Flowering" = "pink3",   
+  "Fruiting"  =  "#F4A636"   
+)
+
+plot_df_bud  <- newdat   |> mutate(stage = "Budding")
+
+plot_df_flower <- newdat_cool_flower |> mutate(stage = "Flowering")
+
+plot_df_fruit <- newdat_cool_fruit |> mutate(stage = "Fruiting")
+
+
+plot_df_all <- bind_rows(
+  plot_df_bud,
+  plot_df_flower,
+  plot_df_fruit
+)
+plot_df_all
+
+
+
+b_f_fr <- ggplot(plot_df_all, aes(x=treat_competition, y=onset, 
+                                    color=stage, shape = site)) +
+  geom_point(position = pd)+
+  facet_wrap(~ region)+
+  geom_errorbar(aes(ymin= plo, ymax= phi), width=.2,
+                position = pd)+
+  scale_color_manual(values = stage_colors)+
+  labs(
+    x = "Biotic interactions",
+    y = "Predicted onset (julian days)",
+    title = "Effect of transplantation on onset across regions"
+  )#+
+  #coord_flip()
+print(b_f_fr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
