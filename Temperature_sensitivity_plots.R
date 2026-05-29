9fbd7b5be89afc2febd5654398e2d434bf7a64c0
Nadi25 <nadine-arzt@gmx.de>
2026-05-29 10:29:59 +0200

    add gdd plot

--- Temperature_sensitivity_plots.R
+++ Temperature_sensitivity_plots.R
@@ -10,14 +10,14 @@ theme_set(theme_bw())
 
 
 # Source temperature sensitivity analysis script --------------------------
-
 source("Temperature_sensitivity_analysis.R")
 
 
 # plot temp sens predictions julian days ----------------------------------------------
 # for both regions 
 # but used separate models
-p_sens <- plot_temp_sens_predictions(sens_all_gs, plot_predictions_gs)
+p_sens <- plot_temp_sens_predictions(sens_all_gs, plot_predictions_gs,
+                                     expression("Temperature sensitivity (days/"*degree*"C)"))
 p_sens
 
 # ggsave(filename = "Output/Sensitivity/Temperature_sensitivity_gs_bud_flower_fruit_seed_onset_NOR_CHE.png", 
@@ -27,8 +27,21 @@ p_sens
 
 
 
+# plot temp sens predictions growing degree days (GDD) ----------------------------------------------
+# for both regions 
+# but used separate models
+p_sens_gdd <- plot_temp_sens_predictions(sens_all_gs_gdd, plot_predictions_gs_gdd, 
+                                         expression("Temperature sensitivity (GDD2/"*degree*"C)"))
+p_sens_gdd
+
+# ggsave(filename = "Output/Sensitivity/Temperature_sensitivity_GDD_gs_bud_flower_fruit_seed_onset_NOR_CHE.png", 
+#       plot = p_sens_gdd,
+#        width = 15, height = 10, units = "in")
 
 
+p_sens_gdd2 <- plot_temp_sens_predictions(sens_all_gs_gdd, plot_predictions_gs_gdd2, 
+                                         expression("Temperature sensitivity (GDD2/"*degree*"C)"))
+p_sens_gdd2
 
 
 
