869e5e40da63d2e1509d73b402f58801a35810bd
Nadi25 <nadine-arzt@gmx.de>
2026-06-02 16:39:05 +0200

    read me phenology scripts

---
+++ Read_me_phenology_NOR_CHE.R
@@ -0,0 +1,83 @@
+
+
+# READ ME -----------------------------------------------------------------
+
+# data preparation scripts
+
+# phenology
+source("Data_preparation_phenology_NOR.R")
+source("Data_preparation_phenology_NOR_CHE_combined.R")
+
+# cliamte stations
+source("Data_preparation_climate_station_NOR.R")
+source("Data_preparation_climate_station_CHE.R")
+
+# tomst 
+source("Data_preparation_TMS4_CHE.R")
+
+# biomass
+source("Data_praparation_biomass_NOR.R")
+source("Data_preparation_traits_biomass_NOR_24.R")
+
+# traits
+source("RangeX_data_paper_cleaning_demographic_traits_23.R")
+
+
+
+# Onset -------------------------------------------------------------------
+
+# julian days
+source("Transplantation_warming_predictions_NOR_CHE.R")
+
+# GDD
+source("GDD_Cooling_bud_flower_fruit_predictions.R")
+
+# GDD vs DOY
+source("GDD_DOY_NOR_CHE.R")
+
+
+
+# Duration ----------------------------------------------------------------
+
+source("Duration_bud_flower_fruit_predictions_NOR_CHE.R")
+
+
+
+# Temperature sensitivity -------------------------------------------------
+
+source("Temperature_sensitivity_functions.R")
+
+source("Temperature_sensitivity_analysis.R")
+
+source("Temperature_sensitivity_plots.R")
+
+
+
+
+# Flower number -----------------------------------------------------------------
+
+# flower number as it is
+source("Flower_number_predictions_transplantation_warming.R")
+
+# predicting biomass for 23
+source("Biomass_traits_correlation_per_species.R")
+source("Biomass_prediction_24_compare_with_real_24.R")
+source("Biomass_prediction_23_per_species.R")
+
+# combine biomass with phenology
+source("Biomass_phenology_combine_species_models.R")
+
+# flower number adjusted for biomass
+source("Biomass_flower_number_predictions.R")
+
+
+
+
+
+
+
+
+
+
+
+
