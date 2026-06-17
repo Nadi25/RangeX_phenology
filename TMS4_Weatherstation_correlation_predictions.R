a15a7aa58d96406ea0087d9249d7c2e4136678b8
Nadi25 <nadine-arzt@gmx.de>
2026-06-17 16:23:09 +0200

    done one model per treat now need to do one per logger

--- TMS4_Weatherstation_correlation_predictions.R
+++ TMS4_Weatherstation_correlation_predictions.R
@@ -1,10 +1,16 @@
 
 
-
+library(conflicted)
+conflict_prefer_all("dplyr", quiet = TRUE)
+library(tidyverse)
+library(performance)
 
 source("Data_preparation_TMS4_NOR.R")
 
 source("Data_preparation_climate_station_NOR.R")
+
+theme_set(theme_bw())
+
 
 # filter year 23 -------------------------------------------------------
 climate_23 <- climate |> 
@@ -76,53 +82,50 @@ compare_temp2 <- env_nor_daily |>
 
 
 
-m_tomst <- lm(T3_mean ~ Tavg + site + treat_warming + treat_competition, data = compare_temp2)
+# m_tomst <- lm(T3_mean ~ Tavg + site + treat_warming + treat_competition, data = compare_temp2)
+# 
+# summary(m_tomst)
+# model_performance(m_tomst)
+# 
+# plot(T3_mean ~ predict(m_tomst),
+#      data = compare_temp2)
+# 
+# 
 
-summary(m_tomst)
-model_performance(m_tomst)
+# 
+# dd <- climate_23_daily |> 
+#   filter(site == "hi")
+# 
+# # predict tomst
+# newdata <- expand.grid(
+#   date = dd$date,
+#   site = c("lo", "hi"),
+#   treat_warming = c("ambi", "warm"),
+#   treat_competition = c("vege", "bare")) |>
+#   filter(!(site == "lo" & treat_warming == "warm"))
+# 
+# 
+# newdata <- newdata |>
+#   left_join(
+#     climate_23_daily,
+#     by = c("date", "site")
+#   )
+# 
+# p <- predict(
+#   m_tomst,
+#   newdata = newdata,
+#   re.form = NA,
+#   se.fit = TRUE) |> 
+#   
+#   as_tibble() |>
+#   mutate(upper = fit + 1.96 * se.fit,
+#          lower = fit - 1.96 * se.fit) |>
+#   bind_cols(newdata)
+# p
 
-plot(T3_mean ~ predict(m_tomst),
-     data = compare_temp2)
-
-
-
-
-dd <- climate_23_daily |> 
-  filter(site == "hi")
-
-# predict tomst
-newdata <- expand.grid(
-  date = dd$date,
-  site = c("lo", "hi"),
-  treat_warming = c("ambi", "warm"),
-  treat_competition = c("vege", "bare")) |>
-  filter(!(site == "lo" & treat_warming == "warm"))
-
-
-newdata <- newdata |>
-  left_join(
-    climate_23_daily,
-    by = c("date", "site")
-  )
-
-
-
-
-p <- predict(
-  m_tomst,
-  newdata = newdata,
-  re.form = NA,
-  se.fit = TRUE) |> 
-  
-  as_tibble() |>
-  mutate(upper = fit + 1.96 * se.fit,
-         lower = fit - 1.96 * se.fit) |>
-  bind_cols(newdata)
-p
-
-ggplot(p, aes(x = Tavg, y = fit, colour = treat_competition)) +
-  geom_point()+
-  facet_wrap(~ site)
+# ggplot(p, aes(x = Tavg, y = fit, colour = treat_competition)) +
+#   geom_point()+
+#   facet_wrap(~ site)
 
 
 
@@ -136,6 +139,7 @@ ggplot(compare_temp2, aes(x = Tavg, y = T3_mean)) +
 
 # one model per treatment -------------------------------------------------
 
+# hi ambi bare
 dat_hi_ambi_bare <- compare_temp2 |>
   filter(
     site == "hi",
@@ -148,7 +152,20 @@ m_hi_ambi_bare <- lm(T3_mean ~ Tavg, data = dat_hi_ambi_bare)
 summary(m_hi_ambi_bare)
 model_performance(m_hi_ambi_bare)
 
+m_hi_ambi_bare2 <- lm(T3_mean ~ Tavg + Radiation, data = dat_hi_ambi_bare)
 
+summary(m_hi_ambi_bare2)
+model_performance(m_hi_ambi_bare2)
+
+m_hi_ambi_bare3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_hi_ambi_bare)
+
+summary(m_hi_ambi_bare3)
+model_performance(m_hi_ambi_bare3)
+
+AIC(m_hi_ambi_bare, m_hi_ambi_bare2, m_hi_ambi_bare3)
+
+
+# hi ambi vege
 dat_hi_ambi_vege <- compare_temp2 |>
   filter(
     site == "hi",
@@ -161,7 +178,22 @@ m_hi_ambi_vege <- lm(T3_mean ~ Tavg, data = dat_hi_ambi_vege)
 summary(m_hi_ambi_vege)
 model_performance(m_hi_ambi_vege)
 
+m_hi_ambi_vege2 <- lm(T3_mean ~ Tavg + Radiation, data = dat_hi_ambi_vege)
 
+summary(m_hi_ambi_vege2)
+model_performance(m_hi_ambi_vege2)
+
+m_hi_ambi_vege3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_hi_ambi_vege)
+
+summary(m_hi_ambi_vege3)
+model_performance(m_hi_ambi_vege3)
+
+AIC(m_hi_ambi_vege, m_hi_ambi_vege2, m_hi_ambi_vege3)
+
+
+
+
+# hi warm vege
 dat_hi_warm_vege <- compare_temp2 |>
   filter(
     site == "hi",
@@ -172,20 +204,24 @@ dat_hi_warm_vege <- compare_temp2 |>
 m_hi_warm_vege <- lm(T3_mean ~ Tavg, data = dat_hi_warm_vege)
 
 summary(m_hi_warm_vege)
+model_performance(m_hi_warm_vege)
 
-m_hi_warm_vege2 <- lm(T3_mean ~ Tavg + Radiation + Radiation, data = dat_hi_warm_vege)
+
+m_hi_warm_vege2 <- lm(T3_mean ~ Tavg + Radiation, data = dat_hi_warm_vege)
 
 summary(m_hi_warm_vege2)
+model_performance(m_hi_warm_vege2)
 
 m_hi_warm_vege3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_hi_warm_vege)
 
 summary(m_hi_warm_vege3)
+model_performance(m_hi_warm_vege3)
 
 
 AIC(m_hi_warm_vege, m_hi_warm_vege2, m_hi_warm_vege3)
 
 
-
+# hi warm bare
 dat_hi_warm_bare <- compare_temp2 |>
   filter(
     site == "hi",
@@ -196,15 +232,23 @@ dat_hi_warm_bare <- compare_temp2 |>
 m_hi_warm_bare <- lm(T3_mean ~ Tavg, data = dat_hi_warm_bare)
 
 summary(m_hi_warm_bare)
+model_performance(m_hi_warm_bare)
+
 
 m_hi_warm_bare2 <- lm(T3_mean ~ Tavg + Radiation, data = dat_hi_warm_bare)
 
 summary(m_hi_warm_bare2)
+model_performance(m_hi_warm_bare2)
 
 m_hi_warm_bare3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_hi_warm_bare)
 
 summary(m_hi_warm_bare3)
+model_performance(m_hi_warm_bare3)
 
+AIC(m_hi_warm_bare, m_hi_warm_bare2, m_hi_warm_bare3)
+
+
+# lo ambi bare
 dat_lo_ambi_bare <- compare_temp2 |>
   filter(
     site == "lo",
@@ -212,19 +256,21 @@ dat_lo_ambi_bare <- compare_temp2 |>
     treat_competition == "bare"
   )
 
-
-AIC(m_hi_warm_bare, m_hi_warm_bare2, m_hi_warm_bare3)
-
-
-
-
-
-
 m_lo_ambi_bare <- lm(T3_mean ~ Tavg, data = dat_lo_ambi_bare)
 
-summary(dat_lo_ambi_bare)
+summary(m_lo_ambi_bare)
+
+m_lo_ambi_bare2 <- lm(T3_mean ~ Tavg + Radiation , data = dat_lo_ambi_bare)
+
+m_lo_ambi_bare3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_lo_ambi_bare)
 
 
+AIC(m_lo_ambi_bare, m_lo_ambi_bare2, m_lo_ambi_bare3)
+model_performance(m_lo_ambi_bare)
+model_performance(m_lo_ambi_bare3)
+
+
+# lo ambi vege
 dat_lo_ambi_vege <- compare_temp2 |>
   filter(
     site == "lo",
@@ -234,9 +280,31 @@ dat_lo_ambi_vege <- compare_temp2 |>
 
 m_lo_ambi_vege <- lm(T3_mean ~ Tavg, data = dat_lo_ambi_vege)
 
-summary(dat_lo_ambi_vege)
+summary(m_lo_ambi_vege)
 
-# predictions
+m_lo_ambi_vege2 <- lm(T3_mean ~ Tavg + Radiation, data = dat_lo_ambi_vege)
+
+m_lo_ambi_vege3 <- lm(T3_mean ~ Tavg + Radiation + WindSpd, data = dat_lo_ambi_vege)
+
+AIC(m_lo_ambi_vege, m_lo_ambi_vege2, m_lo_ambi_vege3)
+model_performance(m_lo_ambi_vege2)
+model_performance(m_lo_ambi_vege3)
+
+
+# check  correlation between T and Radiation
+cor(compare_temp2$Tavg,
+    compare_temp2$Radiation,
+    use = "complete.obs")
+
+cor(compare_temp2$Tavg,
+    compare_temp2$WindSpd,
+    use = "complete.obs")
+
+
+
+
+# predictions -------------------------------------------------------------
+
 new_hi_ambi_vege <- climate_23_daily |>
   filter(site == "hi")
 
@@ -284,29 +352,29 @@ new_lo <- climate_23_daily |> filter(site == "lo")
 
 
 pred_hi_ambi_vege <- predict_tomst(
-  m_hi_ambi_vege, new_hi, "ambi", "vege"
+  m_hi_ambi_vege3, new_hi, "ambi", "vege"
 )
 
 pred_hi_ambi_bare <- predict_tomst(
-  m_hi_ambi_bare, new_hi, "ambi", "bare"
+  m_hi_ambi_bare3, new_hi, "ambi", "bare"
 )
 
 pred_hi_warm_vege <- predict_tomst(
-  m_hi_warm_vege, new_hi, "warm", "vege"
+  m_hi_warm_vege3, new_hi, "warm", "vege"
 )
 
 pred_hi_warm_bare <- predict_tomst(
-  m_hi_warm_bare, new_hi, "warm", "bare"
+  m_hi_warm_bare3, new_hi, "warm", "bare"
 )
 
 
 
 pred_lo_ambi_vege <- predict_tomst(
-  m_lo_ambi_vege, new_lo, "ambi", "vege"
+  m_lo_ambi_vege3, new_lo, "ambi", "vege"
 )
 
 pred_lo_ambi_bare <- predict_tomst(
-  m_lo_ambi_bare, new_lo, "ambi", "bare"
+  m_lo_ambi_bare3, new_lo, "ambi", "bare"
 )
 
 
@@ -359,13 +427,123 @@ model_performance(lm(T3_mean ~ fit, data = validation))
 
 #####
 
-# column name pred or not -------------------------------------------------
+# combine real tomst with predicted tomst -------------------------------------------------
+
+# rename column of pred values
+pred_all2 <- pred_all2  |> 
+  rename(tomst_pred = fit)
+
+
+
+# add column with type (pred or real) -------------------------------------
+# predicted tms data
+pred_all2 <- pred_all2 |>
+  mutate(logger_type = "tms_predicted")
+
+tms_pred_clean <- pred_all2 |>
+  select(date, site, treat_warming, treat_competition, temp_mean = tomst_pred, logger_type)
+
+
+# real tms data
+tms_real_clean <- env_nor_daily |>
+  group_by(date, site, treat_warming, treat_competition) |>
+  summarise(temp_mean = mean(T3_mean, na.rm = TRUE),
+    .groups = "drop") |>
+  mutate(logger_type = "tms_real")
+
+
+
+
+# join tms predicted and real ---------------------------------------------
+tms_pred_real <- bind_rows(tms_pred_clean, tms_real_clean)
+
+
+
+# plot predicted and real -------------------------------------------------
+
+ggplot(tms_pred_real,
+       aes(x = date, y = temp_mean, color = logger_type)) +
+  geom_line() +
+  facet_grid(treat_warming + site ~ treat_competition)
+
+
+ggplot(tms_pred_real,
+       aes(x = date, y = temp_mean, color = logger_type, linetype = site)) +
+  geom_line() +
+  facet_grid(treat_warming  ~ treat_competition)
+
+
+
+# add climate station real data to tms ------------------------------------
+climate_real <- climate_23_daily |>
+  mutate(
+    temp_mean = Tavg,
+    logger_type = "climate_station",
+    treat_warming = NA_character_,
+    treat_competition = NA_character_
+  ) |>
+  select(date, site, treat_warming, treat_competition,
+         temp_mean, logger_type)
 
 
 
 
 
 
+# add climate station to pred and real tms data ---------------------------
+tms_climate_all <- bind_rows(
+  climate_real,
+  tms_pred_real
+)
+
+# plot everything
+ggplot(tms_climate_all,
+       aes(x = date, y = temp_mean, color = logger_type)) +
+  geom_line() +
+  facet_grid(treat_warming ~ site + treat_competition)
+
+
+
+ggplot(tms_climate_all,
+       aes(x = date, y = temp_mean, color = logger_type, linetype = site)) +
+  geom_line() 
+  #facet_grid(treat_warming ~ site + treat_competition)
+
+
+
+
+
+
+# one model per logger ----------------------------------------------------
+compare_temp2 |>
+  distinct(
+    unique_plot_ID,
+    site,
+    treat_warming,
+    treat_competition
+  ) |>
+  arrange(unique_plot_ID)
+
+
+logger_models <- compare_temp2 |>
+  group_by(unique_plot_ID) |>
+  group_split() |>
+  set_names(
+    compare_temp2 |>
+      distinct(unique_plot_ID) |>
+      pull(unique_plot_ID)
+  ) |>
+  map(~ lm(T3_mean ~ Tavg + Radiation + WindSpd, data = .x))
+
+
+names(logger_models)
+
+summary(logger_models[[1]])
+
+logger_data <- compare_temp2 |>
+  filter(unique_plot_ID == names(logger_models)[1])
+
+head(logger_data)
 
 
 
