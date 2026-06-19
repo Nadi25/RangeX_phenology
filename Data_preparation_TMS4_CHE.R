a800b9d933ceaf351d1cc8324feebaa89045f52d
Nadi25 <nadine-arzt@gmx.de>
2026-06-19 14:27:12 +0200

    use all treatmetns instead of pre site

--- Data_preparation_TMS4_CHE.R
+++ Data_preparation_TMS4_CHE.R
@@ -62,7 +62,7 @@ env_che_22_ <- env_che |>
     year = lubridate::year(date_time)
   ) |>
   filter(year == 2022) |>
-  group_by(site, unique_plot_ID, treat_warming, treat_competition) |>
+  group_by(site, unique_plot_ID, treat_warming, treat_competition, added_focals) |>
   summarise(
     start_date = min(date, na.rm = TRUE),
     end_date   = max(date, na.rm = TRUE),
@@ -108,24 +108,59 @@ env_che_22_ |>
 
 # calculate mean per site per day
 # for only plots with focals to make it comaprable hi and lo
-climate_tomst_22_site <- env_che_22 |>
-  filter(
-    treat_warming == "ambi",
-    treat_competition == "vege",
-    added_focals == "wf"
-  ) |>
-  group_by(site, date) |>
-  summarise(
-    Tmax = max(TMS_T3, na.rm = TRUE),
-    Tmin = min(TMS_T3, na.rm = TRUE),
-    .groups = "drop"
-  ) |>
-  mutate(
-    Tavg_tms = (Tmax + Tmin)/2
-  )
+# climate_tomst_22_site <- env_che_22 |>
+#   filter(
+#     treat_warming == "ambi",
+#     treat_competition == "vege",
+#     added_focals == "wf"
+#   ) |>
+#   group_by(site, date) |>
+#   summarise(
+#     Tmax = max(TMS_T3, na.rm = TRUE),
+#     Tmin = min(TMS_T3, na.rm = TRUE),
+#     .groups = "drop"
+#   ) |>
+#   mutate(
+#     Tavg_tms = (Tmax + Tmin)/2
+#   )
+# 
+# climate_tomst_22_site <- climate_tomst_22_site |>
+#   filter(date >= as.Date("2022-01-07"))
+# 
+# 1 hi    2022-03-28  
+# 2 lo    2022-03-26 
 
-climate_tomst_22_site <- climate_tomst_22_site |>
-  filter(date >= as.Date("2022-01-07"))
+
+# calculate daily mean per treatment --------------------------------------
+tms_che_treat <- env_che_22 |>
+  group_by(date, site, treat_warming, treat_competition, added_focals) |>
+  summarise(
+    temp_mean = mean(TMS_T3, na.rm = TRUE),
+    .groups = "drop")
+tms_che_treat
+
+# cut away the first days of the year to not have GDD start in this warm period before being cold again
+tms_che_treat <- tms_che_treat |>
+  filter(date >= as.Date("2022-01-07"),
+         date<= as.Date("2022-10-23")) # same day as in Norway
+
+
+# delete control plots ----------------------------------------------------
+# we only need the plots with focals for now
+tms_final_che <- tms_che_treat |> 
+  filter(added_focals == "wf")
+
+tms_final_che$treatment_site_temp_comp <- paste(tms_final_che$site, tms_final_che$treat_warming,tms_final_che$treat_competition, sep = "_")
+
+
+
+ggplot(tms_final_che,
+       aes(date,
+           temp_mean, colour = treat_competition)) +
+  geom_line()+
+  facet_grid( ~treatment_site_temp_comp)
+
+
 
 
 
@@ -137,16 +172,16 @@ Nconsec <- 5 # number of consecutive days above Tbase to define growing season s
 
 
 # 1) indicator: Tavg_tms > Tbase
-clim_flag_che_tms <- climate_tomst_22_site |> 
-  arrange(site, date) |> 
-  mutate(is_warm = (Tavg_tms > Tbase))
+clim_flag_che_tms <- tms_che_treat |> 
+  arrange(site, treat_warming, treat_competition, date) |> 
+  mutate(is_warm = (temp_mean > Tbase))
 
 
 # find out start of growing season -------------------------------------
 # 2) compute run of Nconsec TRUE per site and find first date
 season_start_che_tms <- clim_flag_che_tms |> 
   filter(date >= as.Date("2022-01-07")) |> 
-  group_by(site) |> 
+  group_by(site, treat_warming, treat_competition) |> 
   mutate(run_N = slider::slide_dbl(is_warm, ~ ifelse(sum(.x) == length(.x), 1, 0),
                                    .before = Nconsec - 1, .complete = TRUE)) |> 
   # run_N == 1 on the last day of a full Nconsec-run of TRUEs
@@ -155,24 +190,30 @@ season_start_che_tms <- clim_flag_che_tms |>
 season_start_che_tms
 
 
-# 1 hi    2022-03-28  
-# 2 lo    2022-03-26 
+# 1 hi    ambi          bare              2022-04-16  
+# 2 hi    ambi          vege              2022-04-14  
+# 3 hi    warm          bare              2022-04-16  
+# 4 hi    warm          vege              2022-04-13  
+# 5 lo    ambi          bare              2022-03-27  
+# 6 lo    ambi          vege              2022-03-27
+
 
 
 # calculate gdd per site ----------------------------------------------
 # 3) join start dates back and calculate GDD from that start in april
-climate_gdd_che_tms <- climate_tomst_22_site |> 
-  left_join(season_start_che_tms, by = "site") |> 
+climate_gdd_che_tms <- tms_final_che |> 
+  left_join(season_start_che_tms, by = c("site", "treat_warming", "treat_competition")) |> 
   filter(!is.na(season_start)) |> 
-  group_by(site) |> 
+  group_by(site, treat_warming, treat_competition) |> 
   arrange(date) |> 
   mutate(
     # only accumulate on/after season_start
-    GDD_day = if_else(date >= season_start, pmax(0, Tavg_tms - Tbase), 0),
+    GDD_day = if_else(date >= season_start, pmax(0, temp_mean - Tbase), 0),
     GDD_cum = cumsum(GDD_day)
   ) |> 
   ungroup()
 climate_gdd_che_tms
+
 
 # rename date to date_measurement --------------------------------------
 # to match phenology
@@ -203,8 +244,15 @@ ggplot(climate_gdd_che_tms,
     title = "Swiss cumulative GDD development"
   ) 
 
-
-
+ggplot(climate_gdd_che_tms,
+       aes(x = jday,
+           y = GDD_cum,
+           color = treatment_site_temp_comp)) +
+  geom_line(size = 1.2) +
+  labs(
+    x = "Day of year (DOY)",
+    y = "Cumulative temperature (GDD2)",
+    title = "Switzerland cumulative GDD")
 
 
 
