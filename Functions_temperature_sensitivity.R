f5af75a6b908f7db1e9758fed717f8d3255ea597
Nadi25 <nadine-arzt@gmx.de>
2026-06-27 12:52:32 +0200

    predict

--- Functions_temperature_sensitivity.R
+++ Functions_temperature_sensitivity.R
@@ -12,7 +12,7 @@
 # first budding date per individual
 # then average per plot
 
-# data = phenology3
+# data = phenology2
 # stage_name = No_Buds, No_FloOpen, No_FloWithrd, No_Seeds
 # onset_type = jday or GDD_cum
 
@@ -33,43 +33,38 @@ get_mean_onset <- function(data, stage_name, onset_type) {
     summarise(
       onset = min(.data[[onset_type]]),
       .groups = "drop"
-    ) #|>
-  
-  # # mean onset across 3 individuals within plot
-  # group_by(
-  #   region, site, treat_competition,
-  #   species, block_ID, unique_plot_ID
-  # ) |>
-  # summarise(
-  #   onset = mean(first_onset),
-  #   .groups = "drop"
-  # )
-}
-
-
-
-# Average across species, site and treat -----------------------------------------
-# this is not needed or at least it doesnt change from get_mean_onset
-# because unique_plot_ID is essentially treat_comp
-# but we still need an average across the plots
-get_plot_onset <- function(onset_data) {
-  onset_data |>
-    group_by(region, treatment_site_temp, treat_competition, species, block_ID, 
-             unique_plot_ID, functional_group) |>
-    summarise(
-      onset = mean(onset, na.rm = TRUE),
-      .groups = "drop"
     )
 }
+
+
+
+# # Average across species, site and treat -----------------------------------------
+# # this is not needed or at least it doesnt change from get_mean_onset
+# # because unique_plot_ID is essentially treat_comp
+# # but we still need an average across the plots
+# get_plot_onset <- function(onset_data) {
+#   onset_data |>
+#     group_by(region, treatment_site_temp, treat_competition, species, block_ID, 
+#              unique_plot_ID, functional_group) |>
+#     summarise(
+#       onset = mean(onset, na.rm = TRUE),
+#       .groups = "drop"
+#     )
+# }
 
 
 # Model -------------------------------------------------------------------
 # separately per region
 # filter sensitivity dataset per region first
-fit_model_sens <- function(onset_data, region_name, stage_name) {
+
+
+# lo ambi vs hi ambi ------------------------------------------------------
+
+fit_model_sens_lo_hi <- function(onset_data, region_name, stage_name, temp_name) {
   onset_region_stage <- onset_data |> 
     filter(region == region_name) |> 
-    filter(stage == stage_name)
+    filter(stage == stage_name) |> 
+    filter(treat_warming == temp_name)
   
   model <- lmerTest::lmer(
     onset ~ treat_competition + Tmean + (1 | species) + (1 | block_ID),
@@ -84,7 +79,7 @@ fit_model_sens <- function(onset_data, region_name, stage_name) {
 
 # Predictions -------------------------------------------------------------
 
-make_sens_predictions <- function(model, onset_data) {
+make_sens_predictions_lh <- function(model, onset_data) {
   
   newdata <- expand.grid(treat_competition = c("with", "without"),
                          Tmean = mean(onset_data$Tmean, na.rm = TRUE)) |> 
@@ -116,7 +111,27 @@ make_sens_predictions <- function(model, onset_data) {
 
 
 
+make_sens_predictions_lh <- function(model, onset_data, temp_data) {
 
+  temp_seq <- temp_data$Tmean
+  
+  newdata <- expand.grid(
+    treat_competition = c("with", "without"),
+    Tmean = temp_seq
+  ) |> as_tibble()
+  
+  pred <- predict(model, newdata = newdata,
+                  re.form = NA, se.fit = TRUE)
+  
+  newdata$fit <- pred$fit
+  newdata$se  <- pred$se.fit
+  
+  newdata |>
+    mutate(
+      upper = fit + 1.96 * se,
+      lower = fit - 1.96 * se
+    )
+}
 
 
 
