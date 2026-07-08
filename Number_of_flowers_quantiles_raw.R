09cddbb3edf3e198fc62c696eb8f11197c1df837
Nadi25 <nadine-arzt@gmx.de>
2026-07-08 21:21:49 +0200

    plot all species

--- Number_of_flowers_quantiles_raw.R
+++ Number_of_flowers_quantiles_raw.R
@@ -52,12 +52,16 @@ phenology_count_nor <- phenology_count_nor |>
 ## calculate quantiles (fits to median) instead of sd to avoid having negative values for mean-/+ sd. 
 ## Negative ranges come when we have many low values and a few high ones
 phenology_median_quant <- phenology_count_nor |> 
-  group_by(species, date_measurement, site, treat_warming, treat_competition, phenology_stage) |> 
+  group_by(species, date_measurement, site, treat_warming, treat_competition, treatment_site_temp, phenology_stage) |> 
   #group_by(species, date, treatment, Stage) |> 
   summarise(median = median(value),
             lo = quantile(value, probs = 0.1, na.rm = TRUE), 
             hi = quantile(value, probs = 0.9, na.rm = TRUE), 
             .groups = "drop")
+
+
+
+# function to filter by species -------------------------------------------
 
 
 
@@ -69,12 +73,130 @@ phenology_clean_median_quant_plalan <- phenology_median_quant |>
 ggplot(phenology_clean_median_quant_plalan, aes(x = date_measurement)) +
   geom_line(aes(y = median, color = phenology_stage)) +
   geom_ribbon(aes(y = median, ymin = lo, ymax = hi, fill = phenology_stage), linewidth = 2, alpha = 0.5) +
-  facet_grid(rows = vars(site), cols = vars(treat_competition)) +  # Added `scales = "free"`
+  facet_grid(rows = vars(treatment_site_temp), cols = vars(treat_competition)) +  # Added `scales = "free"`
   labs(y = "Median +/- quantiles", x = "", title = "Plantago lanceolata") +
   scale_color_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
   scale_fill_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
   theme(legend.position = "top")
 
+
+
+
+# Leuvul ------------------------------------------------------------------
+
+phenology_clean_median_quant_leuvul <- phenology_median_quant |> 
+  filter(species == "leuvul")
+
+
+ggplot(phenology_clean_median_quant_leuvul, aes(x = date_measurement)) +
+  geom_line(aes(y = median, color = phenology_stage)) +
+  geom_ribbon(aes(y = median, ymin = lo, ymax = hi, fill = phenology_stage), linewidth = 2, alpha = 0.5) +
+  facet_grid(rows = vars(treatment_site_temp), cols = vars(treat_competition)) +  # Added `scales = "free"`
+  labs(y = "Median +/- quantiles", x = "", title = "Leucanthemum vulgare") +
+  scale_color_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
+  scale_fill_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
+  theme(legend.position = "top")
+
+
+
+
+
+filter_data_species_aw <- function(phenology_data, species_name) {
+  
+  phenology_median_quant_s <- phenology_data |> 
+    filter(species == "species_name")
+  
+  p <- ggplot(phenology_median_quant_s, aes(x = date_measurement)) +
+    geom_line(aes(y = median, color = phenology_stage)) +
+    geom_ribbon(aes(y = median, ymin = lo, ymax = hi, fill = phenology_stage), linewidth = 2, alpha = 0.5) +
+    facet_grid(rows = vars(treatment_site_temp), cols = vars(treat_competition)) +  # Added `scales = "free"`
+    labs(y = "Median +/- quantiles", x = "", title = "Leucanthemum vulgare") +
+    scale_color_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
+    scale_fill_manual(values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")) +
+    theme(legend.position = "top")
+  
+  
+  return(phenology_median_quant_s)
+  return(p)
+}
+
+plot_phenology_species <- function(data, species_name, species_title) {
+  
+  data |>
+    filter(species == species_name) |>
+    ggplot(aes(x = date_measurement)) +
+    geom_line(aes(y = median, color = phenology_stage)) +
+    geom_ribbon(
+      aes(y = median, ymin = lo, ymax = hi, fill = phenology_stage),
+      alpha = 0.5
+    ) +
+    facet_grid(
+      rows = vars(treatment_site_temp),
+      cols = vars(treat_competition)
+    ) +
+    labs(
+      y = "Median +/- quantiles",
+      x = "",
+      title = species_title
+    ) +
+    scale_color_manual(
+      values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")
+    ) +
+    scale_fill_manual(
+      values = c("#00BB00FF", "#500050FF", "#FFBBFFFF")
+    ) +
+    theme(legend.position = "top")
+}
+
+
+
+p_cyncri <- plot_phenology_species(
+  phenology_median_quant,
+  "cyncri",
+  "Cynosurus cristatus")
+p_cyncri
+
+p_hypmac <- plot_phenology_species(
+  phenology_median_quant,
+  "hypmac",
+  "Hypericum maculatum")
+p_hypmac
+
+p_leuvul <- plot_phenology_species(
+  phenology_median_quant,
+  "leuvul",
+  "Leucanthemum vulgare")
+p_leuvul
+
+p_luzmul <- plot_phenology_species(
+  phenology_median_quant,
+  "luzmul",
+  "Luzula multiflora")
+p_luzmul
+
+p_pimsax <- plot_phenology_species(
+  phenology_median_quant,
+  "pimsax",
+  "Pimpinella saxifraga")
+p_pimsax
+
+p_plalan <- plot_phenology_species(
+  phenology_median_quant,
+  "plalan",
+  "Plantago lanceolata")
+p_plalan
+
+p_sucpra <- plot_phenology_species(
+  phenology_median_quant,
+  "sucpra",
+  "Succisa pratensis")
+p_plalan
+
+p_tripra <- plot_phenology_species(
+  phenology_median_quant,
+  "tripra",
+  "Trifolium pratense")
+p_tripra
 
 
 
