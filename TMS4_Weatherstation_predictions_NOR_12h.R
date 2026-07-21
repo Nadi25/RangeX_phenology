7fea3d5515899341589e24c1d8998e65b5ac6f7d
Nadi25 <nadine-arzt@gmx.de>
2026-07-21 16:12:31 +0200

    rename objects so that other scritps run

--- TMS4_Weatherstation_predictions_NOR_12h.R
+++ TMS4_Weatherstation_predictions_NOR_12h.R
@@ -130,7 +130,7 @@ compare_temp |>
 
 # make one model per logger = unique_plot_ID
 # with daily mean temp from the climate station and radiation and wind speed
-logger_models_12 <- compare_temp |>
+logger_models <- compare_temp |>
   group_by(unique_plot_ID) |>
   group_split() |>
   set_names(
@@ -141,18 +141,18 @@ logger_models_12 <- compare_temp |>
   map(~ lm(Tmean_12h ~ Tmean_12h + Radiation + WindSpd, data = .x))
 
 
-names(logger_models_12)
+names(logger_models)
 
 # check some outputs
-summary(logger_models_12[[1]])
-summary(logger_models_12[[3]])
-summary(logger_models_12[[9]])
-summary(logger_models_12[[20]])
+summary(logger_models[[1]])
+summary(logger_models[[3]])
+summary(logger_models[[9]])
+summary(logger_models[[20]])
 
-logger_data_12 <- compare_temp |>
-  filter(unique_plot_ID == names(logger_models_12)[1])
+logger_data <- compare_temp |>
+  filter(unique_plot_ID == names(logger_models)[1])
 
-head(logger_data_12)
+head(logger_data)
 
 
 
@@ -166,14 +166,14 @@ logger_ids
 length(logger_ids)
 
 # function to predict logger data
-predict_logger_12 <- function(id){
+predict_logger <- function(id){
   
   # data from one logger
   dat_logger <- compare_temp |>
     filter(unique_plot_ID == id)
   
   # model
-  m_logger_12 <- lm(
+  m_logger <- lm(
     T3_mean_12h ~ Tmean_12h + Radiation + WindSpd,
     data = dat_logger
   )
@@ -188,7 +188,7 @@ predict_logger_12 <- function(id){
   
   # prediction
   pred <- predict(
-    m_logger_12,
+    m_logger,
     newdata = newdata,
     se.fit = TRUE
   )
@@ -211,7 +211,7 @@ predict_logger(logger_ids[1])
 
 
 # loops through all loggers
-pred_logger_12 <- map_df(logger_ids, predict_logger_12)
+pred_logger <- map_df(logger_ids, predict_logger)
 
 
 
@@ -221,16 +221,16 @@ pred_logger_12 <- map_df(logger_ids, predict_logger_12)
 
 # add column with type (pred or real) -------------------------------------
 # predicted tms data
-pred_logger2_12 <- pred_logger_12 |>
+pred_logger2 <- pred_logger |>
   mutate(logger_type = "tms_predicted")|> 
   rename(temp_mean = fit)
 
-tms_pred_clean_12 <- pred_logger2_12 |>
+tms_pred_clean <- pred_logger2 |>
   select(date, site, treat_warming, treat_competition, added_focals, temp_mean, unique_plot_ID, logger_type)
 
 
 # real tms data
-tms_real_12 <- tomst_nor_daily |> 
+tms_real <- tomst_nor_daily |> 
   mutate(logger_type = "tms_real")|>
   rename(temp_mean = T3_mean_12h) |> 
   select(date, site, treat_warming, treat_competition, added_focals, temp_mean, unique_plot_ID, logger_type)
@@ -284,16 +284,16 @@ tms_real_12 <- tomst_nor_daily |>
 
 
 # join tms predicted and real --------------------------------------------- 
-tms_pred_real_12 <- bind_rows(tms_pred_clean_12, tms_real_12)
+tms_pred_real <- bind_rows(tms_pred_clean, tms_real)
 
 # plot predicted and real -------------------------------------------------
-ggplot(tms_pred_real_12,
+ggplot(tms_pred_real,
        aes(x = date, y = temp_mean, color = logger_type, linetype = site)) +
   geom_line() +
   facet_grid(treat_warming  + added_focals ~ treat_competition)
 
 
-ggplot(tms_pred_real_12,
+ggplot(tms_pred_real,
        aes(x = date, y = temp_mean, color = logger_type, linetype = site)) +
   geom_line() +
   facet_grid(treat_warming + site ~ treat_competition)
@@ -305,21 +305,21 @@ ggplot(tms_pred_real_12,
 
 # combine pred_logger with tomst_nor_daily
 
-tms_pred_12 <- pred_logger_12 |>
+tms_pred <- pred_logger |>
   mutate(logger_type = "tms_predicted")|> 
   rename(temp_mean_pred = fit) |> 
   select(date, site, treat_warming, treat_competition, added_focals, unique_plot_ID, temp_mean_pred, logger_type)
 
 
-tms_real_12 <- tomst_nor_daily |> 
+tms_real <- tomst_nor_daily |> 
   mutate(logger_type = "tms_real")|>
   rename(temp_mean_real = T3_mean_12h)|> 
   select(date, site, treat_warming, treat_competition, added_focals, unique_plot_ID, temp_mean_real, logger_type)
 
 
-tms_joined_12 <- tms_pred_12 |>
+tms_joined <- tms_pred |>
   left_join(
-    tms_real_12,
+    tms_real,
     by = c(
       "date",
       "site",
@@ -331,7 +331,7 @@ tms_joined_12 <- tms_pred_12 |>
   )
 
 # make new column chosing pred or real and correct logger type column
-tms_joined_12 <- tms_joined_12 |>
+tms_joined <- tms_joined |>
   mutate(temp_mean_used = coalesce(temp_mean_real, temp_mean_pred),
          
          logger_type = if_else(
@@ -341,7 +341,7 @@ tms_joined_12 <- tms_joined_12 |>
 
 
 # select only necessary columns
-tms_joined2_12 <- tms_joined_12 |>
+tms_joined2 <- tms_joined |>
   select(
     date,
     site,
@@ -356,7 +356,7 @@ tms_joined2_12 <- tms_joined_12 |>
 
 
 
-ggplot(tms_joined2_12,
+ggplot(tms_joined2,
        aes(x = date, y = temp_mean_used, color = logger_type)) +
   geom_line() +
   facet_grid(treat_warming + site ~ treat_competition)
@@ -364,14 +364,14 @@ ggplot(tms_joined2_12,
 
 
 
-tms_joined2_12 |>
+tms_joined2 |>
   count(
     unique_plot_ID,
     date
   ) |>
   filter(n > 1)
 
-tms_joined2_12 |>
+tms_joined2 |>
   count(
     date,
     site,
@@ -384,7 +384,7 @@ tms_joined2_12 |>
 # this plot shows per logger 
 # that is why it has some overlap where it is predicted and real at the same time
 # whithin one treatment, loggers were deployed at several dates
-ggplot(tms_joined2_12,
+ggplot(tms_joined2,
        aes(date,
            temp_mean_used,
            colour = logger_type,
@@ -396,7 +396,7 @@ ggplot(tms_joined2_12,
 
 # calculate mean per treatment --------------------------------------------
 # average of several loggers
-tms_final_12 <- tms_joined2_12 |>
+tms_final <- tms_joined2 |>
   group_by(
     date,
     site,
@@ -414,7 +414,7 @@ tms_final_12 <- tms_joined2_12 |>
 # Calculate daily mean per treatment including percentage of real data --------
 # get number of loggers that are using real and predicted tms data
 # and calculate percentage for both
-tms_final_12 <- tms_joined2_12 |>
+tms_final <- tms_joined2 |>
   group_by(
     date,
     site,
@@ -440,14 +440,14 @@ tms_final_12 <- tms_joined2_12 |>
 
 
 # plot used temperature mean per treatment --------------------------------
-ggplot(tms_final_12,
+ggplot(tms_final,
        aes(date,
            temp_mean)) +
   geom_line()+
   facet_grid(treat_warming + site + added_focals ~ treat_competition)
 
 
-ggplot(tms_final_12,
+ggplot(tms_final,
        aes(date,
            temp_mean, colour = treat_competition, linetype = site)) +
   geom_line()+
@@ -456,10 +456,10 @@ ggplot(tms_final_12,
 
 
 # column with site_warming treatment --------------------------------------
-tms_final_12$treatment_site_temp <- paste(tms_final_12$site, tms_final_12$treat_warming, sep = "_")
+tms_final$treatment_site_temp <- paste(tms_final$site, tms_final$treat_warming, sep = "_")
 
 
-tms_final_12 <- tms_final_12|>
+tms_final <- tms_final|>
   mutate(treatment_site_temp= factor(treatment_site_temp,
                                      levels = c("lo_ambi",
                                                 "hi_ambi",
@@ -467,7 +467,7 @@ tms_final_12 <- tms_final_12|>
 
 
 # control plot
-ggplot(tms_final_12,
+ggplot(tms_final,
        aes(date,
            temp_mean, colour = treat_competition, linetype = treatment_site_temp)) +
   geom_line()+
@@ -477,16 +477,16 @@ ggplot(tms_final_12,
 
 # delete control plots ----------------------------------------------------
 # we only need the plots with focals for now
-tms_final_nor_12 <- tms_final_12 |> 
+tms_final_nor <- tms_final |> 
   filter(added_focals == "wf")
 
-tms_final_nor_12$treatment_site_temp_comp <- paste(tms_final_nor_12$site, 
-                                                   tms_final_nor_12$treat_warming,
-                                                   tms_final_nor_12$treat_competition, sep = "_")
+tms_final_nor$treatment_site_temp_comp <- paste(tms_final_nor$site, 
+                                                   tms_final_nor$treat_warming,
+                                                   tms_final_nor$treat_competition, sep = "_")
 
 
 
-ggplot(tms_final_nor_12,
+ggplot(tms_final_nor,
        aes(date,
            temp_mean, colour = treat_competition)) +
   geom_line()+
@@ -497,21 +497,21 @@ ggplot(tms_final_nor_12,
 # Calculate GDD -----------------------------------------------------------
 
 # define Tbase and growing season start  -------------------------
-Tbase <- 2 # base temperature for plants to grow
+Tbase <- 5 # base temperature for plants to grow
 Nconsec <- 5 # number of consecutive days above Tbase to define growing season start
 # 5 days above 5 degrees
 
 
 
 # 1) indicator: Tavg_tms > Tbase
-clim_flag_nor_tms_12 <- tms_final_nor_12 |> 
+clim_flag_nor_tms <- tms_final_nor |> 
   arrange(site, treat_warming, treat_competition, date) |> 
   mutate(is_warm = (temp_mean > Tbase))
-clim_flag_nor_tms_12
+clim_flag_nor_tms
 
 # find out start of growing season -------------------------------------
 # 2) compute run of Nconsec TRUE per site and find first date
-season_start_nor_tms_12 <- clim_flag_nor_tms_12 |> 
+season_start_nor_tms <- clim_flag_nor_tms |> 
   filter(date >= as.Date("2023-01-01")) |> 
   group_by(site, treat_warming, treat_competition) |> 
   mutate(run_N = slider::slide_dbl(is_warm, ~ ifelse(sum(.x) == length(.x), 1, 0),
@@ -519,7 +519,7 @@ season_start_nor_tms_12 <- clim_flag_nor_tms_12 |>
   # run_N == 1 on the last day of a full Nconsec-run of TRUEs
   filter(run_N == 1) |> 
   summarise(season_start = min(date), .groups = "drop")
-season_start_nor_tms_12
+season_start_nor_tms
 
 
 # 1 hi    ambi          bare              2023-04-12  
@@ -532,8 +532,8 @@ season_start_nor_tms_12
 
 # calculate gdd per site ----------------------------------------------
 # 3) join start dates back and calculate GDD from that start in april
-climate_gdd_nor_tms_12 <- tms_final_nor_12 |> 
-  left_join(season_start_nor_tms_12, by = c("site",
+climate_gdd_nor_tms <- tms_final_nor |> 
+  left_join(season_start_nor_tms, by = c("site",
                                          "treat_warming",
                                          "treat_competition")) |> 
   filter(!is.na(season_start)) |> 
@@ -545,21 +545,21 @@ climate_gdd_nor_tms_12 <- tms_final_nor_12 |>
     GDD_cum = cumsum(GDD_day)
   ) |> 
   ungroup()
-climate_gdd_nor_tms_12
+climate_gdd_nor_tms
 
 # rename date to date_measurement --------------------------------------
 # to match phenology
-climate_gdd_nor_tms_12 <- climate_gdd_nor_tms_12 |> 
+climate_gdd_nor_tms <- climate_gdd_nor_tms |> 
   rename("date_measurement" = "date")
 
 
 # get julian day ----------------------------------------------------------
-climate_gdd_nor_tms_12 <- climate_gdd_nor_tms_12 |>
+climate_gdd_nor_tms <- climate_gdd_nor_tms |>
   mutate(jday = lubridate::yday(date_measurement))
 
 
 # control plot ------------------------------------------------------------
-ggplot(climate_gdd_nor_tms_12,
+ggplot(climate_gdd_nor_tms,
        aes(x = date_measurement,
            y = GDD_cum,
            color = treatment_site_temp)) +
@@ -572,7 +572,7 @@ ggplot(climate_gdd_nor_tms_12,
 
 
 
-ggplot(climate_gdd_nor_tms_12,
+ggplot(climate_gdd_nor_tms,
        aes(x = jday,
            y = GDD_cum,
            color = treatment_site_temp_comp,
